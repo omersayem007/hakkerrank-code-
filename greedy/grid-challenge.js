3dbacad65fb143915af4048d17c4f08d875f80f1
omersayem007 <omer_sayem@yahoo.com>
2017-02-18 13:59:07 +0600

    Create grid-challenge.js

---
+++ greedy/grid-challenge.js
@@ -0,0 +1,17 @@
+function grid( iteams,5)
+{
+ return items; 
+}
+
+
+var items=[
+    [ "e", "b","a","c","d" ],
+    [ "f","g","h","i","j" ],
+    [ "o","l","m","k","n" ],
+    [ "t","r","p","q","s" ],
+    [ "x","y","w","u","v" ]
+  
+  ];
+
+
+console.log( grid(items,5 ));
