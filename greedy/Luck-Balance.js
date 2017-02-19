a25b67b255fdfa68c9f93f648bbfe3649fcfad4b
omersayem007 <omer_sayem@yahoo.com>
2017-02-19 22:31:32 +0600

    Create Luck-Balance.js

---
+++ greedy/Luck-Balance.js
@@ -0,0 +1,19 @@
+function luck_balance()
+{
+  
+  
+  
+}
+
+function data(luck,importance)
+{
+  this.luck=luck;
+  this.importance=importance;
+}
+
+var obj={
+  
+  new object(5,1),
+}
+
+console.log(luck_balance(   ))
