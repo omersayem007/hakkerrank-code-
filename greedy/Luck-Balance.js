fdef806d04fd7a10edd27a0b6efc0e04b7bf0a34
omersayem007 <omer_sayem@yahoo.com>
2017-02-21 23:34:27 +0600

    Update Luck-Balance.js

--- greedy/Luck-Balance.js
+++ greedy/Luck-Balance.js
@@ -1,6 +1,34 @@
-function luck_balance(obj)
+function luck_balance(obj , imp)
 {
-  return obj ;
+  var temp=0;
+  if(imp===0)
+  {
+    for(var i=0 ;i<obj.length;i++)
+    {
+      temp+=obj[i].luck;
+    }
+    return temp ;
+  }
+  else
+  
+     var array=[];
+     var sum=0;
+for(var j=0 ;j<obj.length;j++)
+{
+  if(obj[j].importance === 1)
+  {
+    array.push(obj[j].luck);
+    }
+}
+var sorted=[];
+sorted=array.sort();
+for(var k=0;k<imp ; k++)
+{
+  sum+=sorted[k]; 
+}
+
+return sum;
+
 }
 
 function data(luck,importance)
@@ -12,7 +40,19 @@ function data(luck,importance)
 var obj=[
   
   new data(5,1),
-  new data(1,2)
+  new data(2,1),
+   new data(1,1),
+  new data(8,1),
+   new data(10,0),
+  new data(5,0),
+  
 ];
+ 
 
-console.log(luck_balance( obj ));
+
+/*obj.sort(function(a,b)
+{
+  return b.luck-a.luck ; 
+});*/
+
+console.log( luck_balance(array) );
