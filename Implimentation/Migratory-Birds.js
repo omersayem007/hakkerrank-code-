f30e2e4984e17fe5da77e51630dd5cba47bbb698
omersayem007 <omer_sayem@yahoo.com>
2017-04-16 22:41:30 +0600

    Create Migratory-Birds.js

---
+++ Implimentation/Migratory-Birds.js
@@ -0,0 +1,26 @@
+function birds(total_birds,birds_Id){
+
+var start_Id ;
+var max_Id=[];
+var counter=0;
+for( var  i=0 ; i<total_birds ;i++ )
+{
+  start_Id =birds_Id[i];
+  
+    for( var j=0 ; j<total_birds ;j++ )
+      {
+          if( start_Id == birds_Id[j] )
+              {
+                 counter++;
+               }
+               max_Id[start_Id]=counter;
+      }
+  
+}
+return  max_Id ;
+  
+}
+
+
+
+console.log( birds(6,[1,4,4,4,5,3]) );
