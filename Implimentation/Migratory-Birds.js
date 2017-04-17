4bfa03de3f77fb57ebb9610a0923a32554c43ca5
omersayem007 <omer_sayem@yahoo.com>
2017-04-17 12:07:24 +0600

    Update Migratory-Birds.js

--- Implimentation/Migratory-Birds.js
+++ Implimentation/Migratory-Birds.js
@@ -1,26 +1,21 @@
 function birds(total_birds,birds_Id){
-
 var start_Id ;
 var max_Id=[];
 var counter=0;
-for( var  i=0 ; i<total_birds ;i++ )
+
+for( var  i=0 ; i<2 ;i++ )
 {
   start_Id =birds_Id[i];
-  
+ 
     for( var j=0 ; j<total_birds ;j++ )
       {
           if( start_Id == birds_Id[j] )
               {
                  counter++;
-               }
-               max_Id[start_Id]=counter;
+              }
       }
-  
+   max_Id[start_Id]=counter;
 }
-return  max_Id ;
-  
+return  max_Id ; 
 }
-
-
-
 console.log( birds(6,[1,4,4,4,5,3]) );
